a3b7a68ed8c69cff6bcfcc7cc990cfd1033da857
mhchia <kevin.mh.chia@gmail.com>
2023-05-04 18:36:04 +0800

    update ports

--- app/src/constants/zuzalu.ts
+++ app/src/constants/zuzalu.ts
@@ -1,4 +1,4 @@
 export const PCD_GITHUB_URL = "https://github.com/proofcarryingdata/pcd";
 
 // export const PASSPORT_URL = "https://zupass.org";
-export const PASSPORT_URL = "https://test-zupass.mhchia.com:3000";
+export const PASSPORT_URL = "https://zkchat.mhchia.com:3000";
